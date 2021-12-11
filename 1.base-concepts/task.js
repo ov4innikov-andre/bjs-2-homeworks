374b406be4c89481b20017ffc5ea4f9a52b5d452
Andrey <ov4innikov-andre@yandex.ru>
2021-12-11 19:21:07 +0300

    Доработка task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,19 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b) - (4 * a * c);
-  if (d < 0) {return [];}
-  if (d == 0) {x = (-b/(2 * a)); return x;}
-  if (d > 0) {x1 = (-b + Math.sqrt(d) )/(2 * a); 
-    x2 = (-b - Math.sqrt(d) )/(2*a); return [x1,x2]}
-  return arr; // array
+  let d = Math.pow(b,2) - (4 * a * c);
+  if (d < 0) {
+    return [];
+  }
+  if (d === 0) {
+   let x = (-b/(2 * a)); 
+    return [x];
+  }
+  if (d > 0) {
+  let x1 = (-b + Math.sqrt(d) )/(2 * a); 
+  let x2 = (-b - Math.sqrt(d) )/(2 * a); 
+    return [x1,x2];
+  }
 }
 solveEquation();
 
