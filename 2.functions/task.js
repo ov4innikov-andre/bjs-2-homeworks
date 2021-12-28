cd11eaf38bd3ba70cd3d1d79c3eb28b3b7e721f5
Andrey <ov4innikov-andre@yandex.ru>
2021-12-28 20:00:32 +0300

    1zadanie

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,13 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
+  min=-99;
+  max=99;
+  sum=10;
+for (let i=0; i < arr.length; i++) {
+avg = +((sum / arr.length).toFixed(2));	
+	
+}
 
   return { min: min, max: max, avg: avg };
 }
